588eb41d79dc3f66be5468c207f858af60bde865
Matt Beres <mattyberes@gmail.com>
2026-02-28 19:29:24 -0500

    change home title

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { NotFound } from './not-found/not-found';
 
 export const routes: Routes = [
     {
-        path: '', pathMatch: 'full', component: Home, title: 'Matthew Beres - Home'
+        path: '', pathMatch: 'full', component: Home, title: 'Matthew Beres - Full-Stack Developer'
     },
     {
         path: 'projects', component: Projects, title: 'Matthew Beres - Projects'
     },
-    { path: '**', component: NotFound, title: '404 - Page Not Found' },
+    {
+        path: '**', component: NotFound, title: '404 - Page Not Found'
+    }
 ];
